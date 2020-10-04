5f0888540a8293ba0930710ee80a965b8b432f7b
igorhf <igorhf.2@gmail.com>
2020-10-03 21:14:27 -0300

    implementando a opcao de faixa etaria, corrigindo dados recebido pelos campos

--- src/components/Imc.jsx
+++ src/components/Imc.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Form, Row, Col, Button, Alert } from 'react-bootstrap'
+import InputMask from 'react-input-mask';
+
 import './imc.css'
 
 import TabelaResultado from './tabelaResultado'
@@ -12,7 +14,9 @@ export default class Imc extends React.Component {
             altura: "",
             peso: "",
             mensagem: "",
-            imc: 0
+            imc: 0,
+            radio1: false,
+            radio2: false
         }
 
         this.altura = this.altura.bind(this)
@@ -20,20 +24,25 @@ export default class Imc extends React.Component {
         this.calculaImc = this.calculaImc.bind(this)
         this.imcAdulto = this.imcAdulto.bind(this)
         this.imcIdoso = this.imcIdoso.bind(this)
+        this.radio1 = this.radio1.bind(this)
+        this.radio2 = this.radio2.bind(this)
+
+
 
 
 
     }
 
     altura(event) {
+        console.log(event.target.value)
         this.setState({
-            altura: +event.target.value
+            altura: event.target.value
         })
     }
 
     peso(event) {
         this.setState({
-            peso: +event.target.value
+            peso: event.target.value.replace(/[^0-9,.?!]/g, '').replace(',', '.')
         })
     }
 
@@ -90,39 +99,68 @@ export default class Imc extends React.Component {
     }
 
     calculaImc() {
-        const { altura, peso } = this.state
-        if (altura != 0 && peso != 0) {
-            this.imcAdulto()
+        const { altura, peso, radio1, radio2 } = this.state
+        if (altura != 0 && peso != 0 && radio1 != false || radio2 != false) {
+            if (radio1 == true) {
+                this.imcIdoso()
+            } else {
+                this.imcAdulto()
+            }
+            this.setState({
+                mensagem: ""
+            })
+
         } else {
             this.setState({
                 mensagem: (
                     <Alert variant="light">
-                        É preciso preencher os dois campos
+                        É preciso preencher os dois campos, e marcar sua faixa etária
                     </Alert>
                 )
             })
         }
     }
 
+    radio1() {
+        this.setState({
+            radio1: true,
+            radio2: false
+        })
+    }
+
+    radio2() {
+        this.setState({
+            radio1: false,
+            radio2: true
+        })
+    }
+
     render() {
-        const { altura, peso, mensagem, imc } = this.state
+        const { altura, peso, mensagem, imc, radio1, radio2 } = this.state
         return (
             <div>
-                <h1>Calculadora de IMC</h1>
+                <h1>Calculadora IMC</h1>
                 <div className="">
                     <Row className="justify-content-md-center">
                         <Col md="auto">
                             <Form>
                                 <Col xs={12} md={12}>
+                                    <Form.Group className="text-left">
+                                        <Form.Check onChange={this.radio1} checked={radio1} inline label="Idoso: idade > 65 anos" type='radio' /><br />
+                                        <Form.Check onChange={this.radio2} checked={radio2} inline label="Adulto: idade <= 65 anos" type='radio' />
+                                    </Form.Group>
+                                </Col>
+                                <hr />
+                                <Col xs={12} md={12}>
                                     <Form.Group>
                                         <Form.Label>Altura</Form.Label>
-                                        <Form.Control onChange={this.altura} value={altura} type="number" />
+                                        <InputMask className="form-control" mask="9.99" onChange={this.altura} value={altura} />
                                     </Form.Group>
                                 </Col>
                                 <Col xs={12} md={12}>
                                     <Form.Group>
                                         <Form.Label>Peso</Form.Label>
-                                        <Form.Control onChange={this.peso} value={peso} type="number" />
+                                        <Form.Control onChange={this.peso} value={peso} type="text" />
                                     </Form.Group>
                                 </Col>
                                 <Col>
@@ -134,6 +172,7 @@ export default class Imc extends React.Component {
                             </Form>
                         </Col>
                     </Row>
+                    {mensagem}
                     <TabelaResultado imc={imc} mensagem={mensagem}></TabelaResultado>
                 </div>
             </div>
